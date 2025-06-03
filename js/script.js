e467efbbf42214b82b0a0c64507bea6ce7da4c1d
Arnold™ <hello@workflowautomation.expert>
2025-06-03 11:47:06 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,15 +1,28 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const toggleButton = document.getElementById('toggleButton');
-  const diagramContainer = document.getElementById('diagramContainer');
-
-  toggleButton.addEventListener('click', () => {
+  const toggleButton1 = document.getElementById('toggleButton1');
+  const toggleButton2 = document.getElementById('toggleButton2');
+  
+  const diagramContainer1 = document.getElementById('diagramContainer1');
+  const diagramContainer2 = document.getElementById('diagramContainer2');
+  
+  toggleButton1.addEventListener('click', () => {
     // Toggle the display state of the diagram container
-    if (diagramContainer.style.display === "none" || diagramContainer.style.display === "") {
-      diagramContainer.style.display = "block";
-      toggleButton.textContent = "- Close Diagram";
+    if (diagramContainer1.style.display === "none" || diagramContainer1.style.display === "") {
+      diagramContainer1.style.display = "block";
+      toggleButton1.textContent = "- Close Diagram";
     } else {
-      diagramContainer.style.display = "none";
-      toggleButton.textContent = "+ View Diagram";
+      diagramContainer1.style.display = "none";
+      toggleButton1.textContent = "+ View Diagram";
+    }
+  });
+    toggleButton2.addEventListener('click', () => {
+    // Toggle the display state of the diagram container
+    if (diagramContainer2.style.display === "none" || diagramContainer2.style.display === "") {
+      diagramContainer2.style.display = "block";
+      toggleButton2.textContent = "- Close Diagram";
+    } else {
+      diagramContainer2.style.display = "none";
+      toggleButton2.textContent = "+ View Diagram";
     }
   });
 });
