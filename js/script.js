3c010855c476f044da4c84f621e184815a5f0711
Arnold™ <hello@workflowautomation.expert>
2025-06-05 12:37:17 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
   const toggleButton1 = document.getElementById('toggleButton1');
   const toggleButton2 = document.getElementById('toggleButton2');
-  
+  const toggleButton3 = document.getElementById('toggleButton3');  
   const diagramContainer1 = document.getElementById('diagramContainer1');
   const diagramContainer2 = document.getElementById('diagramContainer2');
-  
+  const diagramContainer3 = document.getElementById('diagramContainer3');  
   toggleButton1.addEventListener('click', () => {
     // Toggle the display state of the diagram container
     if (diagramContainer1.style.display === "none" || diagramContainer1.style.display === "") {
@@ -23,6 +23,16 @@ document.addEventListener('DOMContentLoaded', () => {
     } else {
       diagramContainer2.style.display = "none";
       toggleButton2.textContent = "+ View sketch";
+    }
+  });
+      toggleButton3.addEventListener('click', () => {
+    // Toggle the display state of the diagram container
+    if (diagramContainer3.style.display === "none" || diagramContainer3.style.display === "") {
+      diagramContainer3.style.display = "block";
+      toggleButton3.textContent = "- Close sketch";
+    } else {
+      diagramContainer3.style.display = "none";
+      toggleButton3.textContent = "+ View sketch";
     }
   });
 });
